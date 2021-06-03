8c59e1ba16dd1116cec9a770679b63211e5c9414
root <root@DESKTOP-N2R06UU.localdomain>
2021-06-03 10:17:51 +0200

    added load_destination

--- code/classes/node.py
+++ code/classes/node.py
@@ -1,6 +1,14 @@
 
 class Node():
-    def __init__(self, station1):
+    def __init__(self, station1, station2):
         self.station1 = station1
+        self.station2 = station2
+        self.destinations = {}
+
+    def add_destination(self, node):
+        self.destinations[node.station1] = node
+       
+
+
         
         
--- code/classes/stations.py
+++ code/classes/stations.py
@@ -2,10 +2,10 @@ import csv
 from .node import Node
 
 
-
 class Station():
     def __init__(self, source_file):
-        self.station = self.load_station(source_file)
+        self.stations = self.load_station(source_file)
+        self.load_destination = self.load_destination(source_file)
         
     def load_station(self, source_file):
         """
@@ -16,8 +16,30 @@ class Station():
             reader = csv.DictReader(in_file)
 
             for row in reader:
-                stations[row['station1']] = Node(row['station1'], row['station1'])
-
+                stations[row['station1']] = Node(row['station1'], row['station2'])
+        
         print(stations)
+        
+        return stations
+
+    def load_destination(self, source_file):
+        """
+        Load all the destinations into the loaded nodes.
+        """
+        with open(source_file, 'r') as in_file:
+            reader = csv.DictReader(in_file)
+
+            for row in reader:
+                destinations = []
+                destinations.append(row['station2'])
+                
+                print(destinations)
+
+                station1 = row['station1']
+
+                # Add the destination to the correct node
+                for destination in destinations:
+                    destination = self.stations[destination]
+                    self.stations[station1].add_destination(destination)
 
     
