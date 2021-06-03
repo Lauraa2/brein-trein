f4ca3420f15b1e4ecadc09d67d2083e6c326f260
Susanne-Becker <susanne.becker@hva.nl>
2021-06-03 15:50:53 +0200

    changes

--- code/classes/node.py
+++ code/classes/node.py
@@ -1,14 +1,10 @@
-
 class Node():
-    def __init__(self, station1, station2, distance):
-        self.station1 = station1
-        self.station2 = station2
-        self.distance = distance
+    def __init__(self, station):
+        self.station = station
         self.destinations = {}
 
     def add_destination(self, node):
         self.destinations[node.station1] = node
-       
 
 
         
--- code/classes/stations.py
+++ code/classes/stations.py
@@ -12,21 +12,25 @@ class Station():
         Load all the nodes into the graph.
         """
         stations = {}
-        #destinations = {}
+        destinations = {}
         with open(source_file, 'r') as in_file:
             reader = csv.DictReader(in_file)
 
             for row in reader:
-                stations[row['station1'], row['station2'], row['distance']] = Node(row['station1'], row['station2'], row['distance'])
-                #destinations[row['station2']] = Node(row['station1'], row['station2'])
-        
 
-        #print(stations)
-        for key, value in stations.items():
-            print(key[0])
-            print(key[1])
-            print(key[2])
-            print(value)
+                stations[row['station1']] = Node(row['station1'])
+                stations[row['station2']] = Node(row['station2'])
+                #destinations[row['station2']] = Node(row['station2'])
+            
+        #stations.update(destinations)
+
+        print(len(stations))
+        #print(destinations)
+        #for key, value in stations.items():
+         #   print(key[0])
+          #  print(key[1])
+           # print(key[2])
+            #print(value)
         #print(destinations)
         
         return stations
