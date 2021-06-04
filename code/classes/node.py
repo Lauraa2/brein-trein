2e6d2e4bd50bb85108d3f43d57ac2526ee61aa3d
Lauraa2 <lauracom2112@gmail.com>
2021-06-04 14:07:42 +0200

    deleted add distance

--- code/classes/node.py
+++ code/classes/node.py
@@ -2,15 +2,11 @@ class Node():
     def __init__(self, station):
         self.station = station
         self.connections = {}
-        self.distances = {}
         
 
     def add_connection(self, station, connection, distance):
         self.connections[station] = (connection, distance)
         print(self.connections)
-    
-    def add_distances(self,station):
-        self.distances[station.station] = station
 
 
     
