9d3da3f9caa1e0557ee0d5ccc2cb7f09ad95238b
Lauraa2 <lauracom2112@gmail.com>
2021-06-07 16:54:21 +0200

    added counting duration

--- code/classes/routes.py
+++ code/classes/routes.py
@@ -7,15 +7,21 @@ class Routes():
         self.routes = self.get_random_routes()
 
     def get_random_routes(test):
+        duration = 0
         stations =[]
         networks = list(test.items())
         station = random.choice(networks)
         stations.append(station)
 
-        next = list(station[1].connections)
+        next = list(station[1].connections.items())
+        print(next)
 
         next_random = random.choice(next)
         stations.append(next_random)
+
+        duration += int(next_random[1])
+
+        print(duration)
 
         print(station)
         print(next_random)
