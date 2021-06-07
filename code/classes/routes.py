33837410caf08ab78d216363e244f83c7c7745a6
Lauraa2 <lauracom2112@gmail.com>
2021-06-07 16:24:31 +0200

    updated get_random_station

--- code/classes/routes.py
+++ code/classes/routes.py
@@ -3,19 +3,22 @@ import random
 #from .network import Network
 
 class Routes():
-    def __init__(self, start, destination, duration):
-        self.start = start
-        self.destination = destination
-        self.duration = int(duration)
-        
-        self.stations = []
-        self.get_random_station()
+    def __init__(self):
+        self.routes = self.get_random_station()
 
     def get_random_station(test):
-        entry_list = list(test.items())
-        station = random.choice(entry_list)
+        stations =[]
+        networks = list(test.items())
+        station = random.choice(networks)
         stations.append(station)
-      
+
+        next = list(station[1].connections)
+
+        next_random = random.choice(next)
+        stations.append(next_random)
+
+        print(station)
+        print(next_random)
         print(stations)
 
     #def add_random_station(self, start):
