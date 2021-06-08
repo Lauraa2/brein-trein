47b8b40f7307a26ca3ad8117bbb2da1a01f5aad1
Susanne-Becker <susanne.becker@hva.nl>
2021-06-08 14:20:36 +0200

    routes.py next random

--- code/classes/routes.py
+++ code/classes/routes.py
@@ -7,47 +7,32 @@ class Routes():
         self.routes = self.get_random_routes()
 
     def get_random_routes(test):
-        stack = [""]
-        #counter = 0
-        #while counter <= 100:
-        while len(stack) > 0:
-            duration = 0
-            #counter = 0
-            stations =[]
-            networks = list(test.items())
-            station = random.choice(networks)
-            stations.append(station)
+        duration = 0
+        stations =[]
+        networks = list(test.items())
+        station = random.choice(networks)
+        stations.append(station)
 
-            while duration <= 120:
-                #if counter <= 7:
-                next = list(stations[-1][1].connections.items())
-                    #print(next)
-                next_random = random.choice(next)
-                #print(next_random[0])
+        while duration <= 120:
+            next = list(stations[-1][1].connections.items())
+            next_random = random.choice(next)
 
-                #print(stations[0])
+            for station in stations: 
+                while next_random[0] == station[0]:
+                    if len(next) >= 2:
+                        next_random = random.choice(next)
+                    else:
+                        next_random = next[0]
+                        break
 
-                for station in stations: 
-                    while next_random[0] == station[0]:
-                        #print(station[0])
-                        #next.remove(next_random)
-                        #print(next)
-                        if len(next) >= 2:
-                            next_random = random.choice(next)
-                        else:
-                            next_random = next[0]
-                            #print(next_random)
+            for network in networks:
+                if next_random[0] == network[0]:
+                    stations.append(network)
 
-                for network in networks:
-                    if next_random[0] == network[0]:
-                        stations.append(network)
+            duration += int(next_random[1])
+           
+        print(stations)
 
-                duration += int(next_random[1])
-            #counter += 1
-
-            #print(duration)
-            print(stations)
-
-        
+    
 
 
