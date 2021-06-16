7461c375ac2ba8cca7ce96ae5433d6038c65c46e
Emalisaa <emalisa.antonioli@student.uva.nl>
2021-06-16 16:44:30 +0200

    check connections in hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -25,7 +25,7 @@ class HillClimber:
         self.new_routes.remove_route(random_route)
         self.new_routes.update_duration(- random_route.duration)
 
-        # render a new random route
+        # render a new random route 
         new_single_route = random_alg.get_random_route(self.data_stations, self.max_time_route)
 
         # append the new route to the list of routes and update the duration
@@ -35,6 +35,7 @@ class HillClimber:
     def compare_score(self):
         """
         Checks and accepts better solutions than the current solution.
+        Judgement is based on quality score.
         """
         # calculate the score of the old and the new route
         old_score = self.best_routes.calculate_score()
@@ -47,10 +48,24 @@ class HillClimber:
         if new_score > old_score:
             self.best_routes = self.new_routes
 
-    def run(self, iterations):
+    def compare_connections(self):
+        old_fraction = self.best_routes.calculate_fraction_connections()
+        new_fraction = self.new_routes.calculate_fraction_connections()
+
+        print(old_fraction)
+        print(new_fraction)
+
+        if new_fraction > old_fraction:
+            self.best_routes = self.new_routes
+
+    def run(self, iterations, type):
         """
         Runs the hillclimber algorithm for a specific amount of iterations.
         """
+        if type != 'score' and type != 'connections':
+            print('ERROR: for hillclimber choose either \'score\' or \'connections\'')
+            exit(1)
+        
         for iteration in range(iterations):
             # create a copy of the graph to simulate the change
             self.new_routes = copy.deepcopy(self.best_routes) # Dit self. gemaakt omdat het overal wordt opgeroepen
@@ -59,7 +74,16 @@ class HillClimber:
             self.mutate_single_route()
 
             # accept it if it is better
-            self.compare_score()
+            if type == 'score':
+                self.compare_score()
+            elif type == 'connections':
+                self.compare_connections()
+                if self.best_routes.check_all_connections():
+                    self.best_routes.calculate_score()
+                    return self.best_routes
+        
+        if type == 'connections' and not self.best_routes.check_all_connections():
+            print('Failed to cover all connections ):')
         
         # return the final route with the highest score
         return self.best_routes
