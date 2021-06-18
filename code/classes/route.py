efeb3093c9d5e88bb26a9b2b574785f119bc3dc3
Susanne-Becker <susanne.becker@hva.nl>
2021-06-18 09:39:59 +0200

    remove print statements

--- code/classes/route.py
+++ code/classes/route.py
@@ -20,10 +20,6 @@ class Route():
         Returns True if as station is already present in the route, else False
         """
         for station in self.stations:
-            print(" ")
-            print(station)
-            print(given_station)
-            print(" ")
             if station == given_station:
                 return True
         return False
